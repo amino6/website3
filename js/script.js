a406f8a34201a5b37aa9d6e519e38de062a5f672
hakmi amine <hakmi5508@gmail.com>
2021-01-16 10:15:16 -0800

    webp browser support v2

--- js/script.js
+++ js/script.js
@@ -34,3 +34,37 @@ function enableScroll() {
         document.querySelector(el).style.overflow = "visible";
     });
 }
+
+// webp support for browsers
+const isIE = /*@cc_on!@*/false || !!document.documentMode;
+const isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification));
+let webp_imgs = document.querySelectorAll('.webp-img-check');
+
+// missing forEach on NodeList for IE11
+if (window.NodeList && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = Array.prototype.forEach;
+}
+
+if(isIE || isSafari) {
+    
+    // replace webp images in html
+    const script1 = document.createElement('script');
+    script1.setAttribute('src','https://unpkg.com/webp-hero@0.0.0-dev.27/dist-cjs/polyfills.js');
+    const script2 = document.createElement('script');
+    script2.setAttribute('src','https://unpkg.com/webp-hero@0.0.0-dev.27/dist-cjs/webp-hero.bundle.js');
+    const head = document.querySelector('head');
+    head.appendChild(script1);
+    script1.addEventListener('load',function(e) {
+        head.appendChild(script2);
+        script2.addEventListener('load',function() {
+            const webpMachine = new webpHero.WebpMachine();
+            webpMachine.polyfillDocument();
+        });
+    });
+}
+
+// replace webp images in css
+webp_imgs.forEach(img => {
+    let url = img.currentStyle || window.getComputedStyle(img, false).backgroundImage.replace(/"/g, "");
+    img.style.backgroundImage = url.backgroundImage.replace("webp","jpg");
+});
