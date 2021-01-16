3a104808416cc52c234f770109968d48f6973c56
hakmi amine <hakmi5508@gmail.com>
2021-01-16 10:23:52 -0800

    small bug fix

--- js/script.js
+++ js/script.js
@@ -66,8 +66,9 @@ if(isIE || isSafari) {
 // replace webp images in css
 webp_imgs.forEach(img => {
     let url = img.currentStyle || window.getComputedStyle(img, false).backgroundImage.replace(/"/g, "");
+    
     if(url === img.currentStyle)
         img.style.backgroundImage = url.backgroundImage.replace("webp","jpg");
-    else
+    elseif(url === window.getComputedStyle(img, false).backgroundImage.replace(/"/g, ""))
         img.style.backgroundImage = url.replace("webp","jpg");
 });
